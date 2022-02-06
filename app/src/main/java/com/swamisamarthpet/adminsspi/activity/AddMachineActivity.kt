3f5b23dc75b53166d61fac59e63218a5207f1f2d
Soham-03 <sohamparab1003@gmail.com>
2022-02-06 17:17:32 +0530

    Added add machine activity

---
+++ app/src/main/java/com/swamisamarthpet/adminsspi/activity/AddMachineActivity.kt
@@ -0,0 +1,12 @@
+package com.swamisamarthpet.adminsspi.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.swamisamarthpet.adminsspi.R
+
+class AddMachineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_machine)
+    }
+}
